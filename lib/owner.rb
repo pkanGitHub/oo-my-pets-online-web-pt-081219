29b75e807df354b73b9fe03d498a5e27bdaa8041
Phyllis Kan <pyk9q7@gmail.com>
2019-10-02 04:42:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,60 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    puts "I am a #{self.species}."
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats 
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat
+    Cat.new(name, self)
+  end
+
+  def buy_dog
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dog.each {|dog| dog.mood == "happy"}
+  end
+
+  def feed_cats
+    self.cat.each {|cat| cat.mood == "happy"}
+  end
+
+  def sell_pets
+    
+  end
+
+  def list_pets
+
+  end
 end
