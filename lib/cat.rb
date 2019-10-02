43150006b1d9971808bad5023238530ced0a1bf2
Phyllis Kan <pyk9q7@gmail.com>
2019-10-02 04:20:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,9 +3,16 @@ class Cat
   attr_accessor :mood, :owner
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
-    @mood =
+    @mood = "nervous"
     @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
