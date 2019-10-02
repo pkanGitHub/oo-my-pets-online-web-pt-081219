b9ad288f53f5c2f6ac897caa8341a1a20c8bcf3e
Phyllis Kan <pyk9q7@gmail.com>
2019-10-02 07:04:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,6 +60,6 @@ class Owner
   end
 
   def list_pets
-    "I have #{self.dog.count} dog(s), and #{self.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
