2cd5229aa5144db6f5d3da6446d8b8e41db85a3e
Phyllis Kan <pyk9q7@gmail.com>
2019-10-02 04:48:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,11 +43,11 @@ class Owner
   end
 
   def walk_dogs
-    @@all.each {|dog| dog.mood == "happy"}
+    self.dogs.each {|dog| dog.mood == "happy"}
   end
 
   def feed_cats
-    @@all.each {|cat| cat.mood == "happy"}
+    self.cats.each {|cat| cat.mood == "happy"}
   end
 
   def sell_pets
