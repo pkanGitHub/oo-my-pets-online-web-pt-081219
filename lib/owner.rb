17a9e94d242c1ec27215216c84ca69ea136f4f1a
Phyllis Kan <pyk9q7@gmail.com>
2019-10-02 04:43:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,11 +34,11 @@ class Owner
     Dog.all.select {|dog| dog.owner == self}
   end
 
-  def buy_cat
+  def buy_cat(name)
     Cat.new(name, self)
   end
 
-  def buy_dog
+  def buy_dog(name)
     Dog.new(name, self)
   end
 
